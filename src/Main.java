e11881cc243398a69ac7929a5d012448b9970ec0
Florian Dorau <fdorau@it-economics.de>
2019-06-06 07:49:56 +0200

    Little fixes

--- src/Main.java
+++ src/Main.java
@@ -9,7 +9,7 @@ import java.net.URISyntaxException;
 public class Main {
 
     public static void main(final String[] args) throws AuthorizationException, URISyntaxException {
-        final URI authorizationEndpoint = new URI("https://login.microsoftonline.com/db8e2f82-8a37-4c09-b7de-ed06547b5a20/oauth2/v2.0/authorize");
+        final URI authorizationEndpoint = new URI("https://login.microsoftonline.com/organizations/oauth2/v2.0/authorize");
         final URI redirectUri = new URI("http://localhost:8080/login/oauth2/code/azure");
 
         final UserAgent userAgent = new UserAgentImpl();
